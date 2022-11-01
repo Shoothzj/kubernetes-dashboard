54a8fffb50014485cbb81fa9787921d2fd2b4604
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2022-11-01 18:30:47 +0800

    Add k8s config (#1)

--- src/main/java/com/github/shoothzj/kdash/Main.java
+++ src/main/java/com/github/shoothzj/kdash/Main.java
@@ -1,5 +1,30 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.github.shoothzj.kdash;
 
+import com.github.shoothzj.kdash.config.K8sConfig;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +35,9 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 public class Main {
@@ -34,6 +62,16 @@ public class Main {
     @Bean
     RouterFunction<ServerResponse> staticResourceRouter() {
         return RouterFunctions.resources("/**", new FileSystemResource(staticPath));
+    }
+
+    @Bean
+    ApiClient createApiClient(@Autowired K8sConfig k8sConfig) throws IOException {
+        ApiClient apiClient = ClientBuilder.kubeconfig(
+                KubeConfig.loadKubeConfig(
+                        new FileReader(k8sConfig.kubeConfig, StandardCharsets.UTF_8)))
+                .build();
+        Configuration.setDefaultApiClient(apiClient);
+        return apiClient;
     }
 
     public static void main(String[] args) {
