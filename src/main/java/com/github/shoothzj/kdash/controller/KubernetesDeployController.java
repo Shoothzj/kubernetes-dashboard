8219acd008ba31ad6d48f302d392c5414cd99761
Lico-Tom <583566138@qq.com>
2022-11-03 21:41:20 +0800

    support delete deployment.

--- src/main/java/com/github/shoothzj/kdash/controller/KubernetesDeployController.java
+++ src/main/java/com/github/shoothzj/kdash/controller/KubernetesDeployController.java
@@ -1,11 +1,15 @@
 package com.github.shoothzj.kdash.controller;
 
 import com.github.shoothzj.kdash.module.CreateDeploymentReq;
+import com.github.shoothzj.kdash.module.DeleteDeploymentReq;
 import com.github.shoothzj.kdash.service.KubernetesDeployService;
+import io.kubernetes.client.openapi.ApiException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +30,13 @@ public class KubernetesDeployController {
     public ResponseEntity<Void> createDeployment(@RequestBody CreateDeploymentReq req) throws Exception {
         deployService.createNamespacedDeployment(req);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/deployment/{namespace}")
+    public ResponseEntity<Void> deleteDeploy(@PathVariable String namespace,
+                                             @RequestBody DeleteDeploymentReq req) throws ApiException {
+        deployService.deleteDeploy(namespace, req);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
