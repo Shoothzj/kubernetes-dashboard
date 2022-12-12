a007110c2195095009e3fad45addefec67e4664d
ZhangJian He <shoothzj@gmail.com>
2022-12-12 18:43:51 +0800

    forbid use of lombok data annotation

---
+++ src/main/java/com/github/shoothzj/kdash/module/keepalive/CreateKeepAliveReq.java
@@ -0,0 +1,12 @@
+package com.github.shoothzj.kdash.module.keepalive;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CreateKeepAliveReq {
+
+    public CreateKeepAliveReq() {
+    }
+}
