bb81c910376dc872b34ad5487a3828264d752672
xiang hu <xiang092689@qq.com>
2022-12-19 15:47:04 +0800

    hc changeos impl

--- src/main/java/com/github/shoothzj/kdash/service/hc/HcEcsService.java
+++ src/main/java/com/github/shoothzj/kdash/service/hc/HcEcsService.java
@@ -23,12 +23,34 @@ import com.github.shoothzj.kdash.config.CloudConfig;
 import com.github.shoothzj.kdash.config.HuaweiCloudConfig;
 import com.huaweicloud.sdk.core.auth.BasicCredentials;
 import com.huaweicloud.sdk.ecs.v2.EcsAsyncClient;
+import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersOption;
+import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersRequest;
+import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersRequestBody;
+import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersResponse;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitOption;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitRequest;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitRequestBody;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitResponse;
+import com.huaweicloud.sdk.ecs.v2.model.ListServersDetailsRequest;
+import com.huaweicloud.sdk.ecs.v2.model.ListServersDetailsResponse;
+import com.huaweicloud.sdk.ecs.v2.model.ServerId;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 public class HcEcsService {
     private final EcsAsyncClient ecsClient;
+
+    public static final int RETRY_PAUSE_TIME = 10000;
 
     public HcEcsService(@Autowired CloudConfig cloudConfig, @Autowired HuaweiCloudConfig config) {
         ecsClient = EcsAsyncClient.newBuilder()
@@ -39,5 +61,69 @@ public class HcEcsService {
                         .withProjectId(config.projectId)
                 )
                 .build();
+    }
+
+    public CompletableFuture<ListServersDetailsResponse> getEcsServerList(ListServersDetailsRequest request) {
+        return ecsClient.listServersDetailsAsync(request);
+    }
+
+    public CompletableFuture<BatchStopServersResponse> stopServers(List<String> serverIds) {
+        BatchStopServersRequest request = new BatchStopServersRequest();
+        BatchStopServersRequestBody body = new BatchStopServersRequestBody();
+        BatchStopServersOption osStop = new BatchStopServersOption();
+        List<ServerId> serverIdList = serverIds.stream()
+                .map(id -> new ServerId().withId(id)).collect(Collectors.toList());
+        osStop.setServers(serverIdList);
+        body.setOsStop(osStop);
+        request.setBody(body);
+        return ecsClient.batchStopServersAsync(request);
+    }
+
+    public CompletableFuture<Void> changeos(List<String> serverIds, String imageId) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        // stop servers. this function only send stop command, do not sync server status
+        stopServers(serverIds).whenComplete((stopServersResponse, e) -> {
+            if (e != null) {
+                throw new RuntimeException(e);
+            }
+            // server status need time to sync
+            // changeos
+            List<CompletableFuture<ChangeServerOsWithoutCloudInitResponse>> futures = new ArrayList<>();
+            for (String serverId : serverIds) {
+                ChangeServerOsWithoutCloudInitRequest changeServerOsRequest =
+                        new ChangeServerOsWithoutCloudInitRequest();
+                changeServerOsRequest.setServerId(serverId);
+                ChangeServerOsWithoutCloudInitRequestBody body = new ChangeServerOsWithoutCloudInitRequestBody();
+                ChangeServerOsWithoutCloudInitOption option = new ChangeServerOsWithoutCloudInitOption();
+                option.setImageid(imageId);
+                body.setOsChange(option);
+                changeServerOsRequest.setBody(body);
+                futures.add(ecsClient.changeServerOsWithoutCloudInitAsync(changeServerOsRequest)
+                        .exceptionally(t -> {
+                            log.info("it seems that ecs server status not sync yet, let's try again after {}s",
+                                    RETRY_PAUSE_TIME / 1000);
+                            if (t instanceof CompletionException completionException
+                                    && completionException.getMessage().contains("Ecs.0100")) {
+                                try {
+                                    Thread.sleep(RETRY_PAUSE_TIME);
+                                    futures.add(
+                                            ecsClient.changeServerOsWithoutCloudInitAsync(changeServerOsRequest));
+                                } catch (InterruptedException exc) {
+                                    throw new RuntimeException(exc);
+                                }
+                            }
+                            return null;
+                        }));
+            }
+            for (CompletableFuture<ChangeServerOsWithoutCloudInitResponse> responseFuture : futures) {
+                try {
+                    responseFuture.get();
+                } catch (InterruptedException | ExecutionException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+            future.complete(null);
+        });
+        return future;
     }
 }
