6bf6f13074f46d65d453062ef69526f608eebe29
xiang092689 <58943501+xiang092689@users.noreply.github.com>
2023-01-02 21:25:15 +0800

    1.create and delete ecs severs (#65)

--- src/main/java/com/github/shoothzj/kdash/service/hc/HcEcsService.java
+++ src/main/java/com/github/shoothzj/kdash/service/hc/HcEcsService.java
@@ -21,19 +21,28 @@ package com.github.shoothzj.kdash.service.hc;
 
 import com.github.shoothzj.kdash.config.CloudConfig;
 import com.github.shoothzj.kdash.config.HuaweiCloudConfig;
-import com.github.shoothzj.kdash.util.CommonUtil;
+import com.github.shoothzj.kdash.module.hc.HcCreateServersRequest;
 import com.huaweicloud.sdk.core.auth.BasicCredentials;
 import com.huaweicloud.sdk.ecs.v2.EcsAsyncClient;
-import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersOption;
-import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersRequest;
-import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersRequestBody;
-import com.huaweicloud.sdk.ecs.v2.model.BatchStopServersResponse;
-import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitOption;
-import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitRequest;
-import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitRequestBody;
-import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithoutCloudInitResponse;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithCloudInitOption;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithCloudInitRequest;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithCloudInitRequestBody;
+import com.huaweicloud.sdk.ecs.v2.model.ChangeServerOsWithCloudInitResponse;
+import com.huaweicloud.sdk.ecs.v2.model.CreatePostPaidServersRequest;
+import com.huaweicloud.sdk.ecs.v2.model.CreatePostPaidServersRequestBody;
+import com.huaweicloud.sdk.ecs.v2.model.CreatePostPaidServersResponse;
+import com.huaweicloud.sdk.ecs.v2.model.DeleteServersRequest;
+import com.huaweicloud.sdk.ecs.v2.model.DeleteServersRequestBody;
 import com.huaweicloud.sdk.ecs.v2.model.ListServersDetailsRequest;
 import com.huaweicloud.sdk.ecs.v2.model.ListServersDetailsResponse;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServer;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerDataVolume;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerEip;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerEipBandwidth;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerNic;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerPublicip;
+import com.huaweicloud.sdk.ecs.v2.model.PostPaidServerRootVolume;
+import com.huaweicloud.sdk.ecs.v2.model.ServerDetail;
 import com.huaweicloud.sdk.ecs.v2.model.ServerId;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,17 +51,12 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class HcEcsService {
     private final EcsAsyncClient ecsClient;
-
-    public static final int RETRY_PAUSE_TIME = 10000;
 
     public HcEcsService(@Autowired CloudConfig cloudConfig, @Autowired HuaweiCloudConfig config) {
         ecsClient = EcsAsyncClient.newBuilder()
@@ -69,59 +73,89 @@ public class HcEcsService {
         return ecsClient.listServersDetailsAsync(request);
     }
 
-    public CompletableFuture<BatchStopServersResponse> stopServers(List<String> serverIds) {
-        BatchStopServersRequest request = new BatchStopServersRequest();
-        BatchStopServersRequestBody body = new BatchStopServersRequestBody();
-        BatchStopServersOption osStop = new BatchStopServersOption();
-        List<ServerId> serverIdList = serverIds.stream()
-                .map(id -> new ServerId().withId(id)).collect(Collectors.toList());
-        osStop.setServers(serverIdList);
-        body.setOsStop(osStop);
-        request.setBody(body);
-        return ecsClient.batchStopServersAsync(request);
+    public CompletableFuture<Void> changeos(String serverName, String imageId) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+        ecsClient.listServersDetailsAsync(new ListServersDetailsRequest().withName(serverName)).whenComplete(
+                ((listServersDetailsResponse, ex) -> {
+                    if (ex != null) {
+                        throw new RuntimeException(ex);
+                    }
+                    List<String> serverIds = listServersDetailsResponse.getServers().stream()
+                            .map(ServerDetail::getId).toList();
+                    List<CompletableFuture<ChangeServerOsWithCloudInitResponse>> tasks = new ArrayList<>();
+                    for (String serverId : serverIds) {
+                        ChangeServerOsWithCloudInitRequest changeServerOsRequest =
+                                new ChangeServerOsWithCloudInitRequest();
+                        changeServerOsRequest.setServerId(serverId);
+                        ChangeServerOsWithCloudInitRequestBody body = new ChangeServerOsWithCloudInitRequestBody();
+                        ChangeServerOsWithCloudInitOption option = new ChangeServerOsWithCloudInitOption();
+                        option.setImageid(imageId);
+                        option.setMode("withStopServer");
+                        body.setOsChange(option);
+                        changeServerOsRequest.setBody(body);
+                        tasks.add(ecsClient.changeServerOsWithCloudInitAsync(changeServerOsRequest));
+                    }
+                    for (CompletableFuture<ChangeServerOsWithCloudInitResponse> task : tasks) {
+                        try {
+                            task.get();
+                        } catch (InterruptedException | ExecutionException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    future.complete(null);
+                }));
+        return future;
     }
 
-    public CompletableFuture<Void> changeos(List<String> serverIds, String imageId) {
+    public CompletableFuture<CreatePostPaidServersResponse> createServer(HcCreateServersRequest req) {
+        CreatePostPaidServersRequest createServersRequest = new CreatePostPaidServersRequest();
+        CreatePostPaidServersRequestBody body = new CreatePostPaidServersRequestBody();
+        PostPaidServer server = new PostPaidServer();
+        server.setName(req.getName());
+        server.addDataVolumesItem(new PostPaidServerDataVolume()
+                .withSize(req.getDataVolumeSize())
+                .withVolumetype(req.getDataVolumeType()));
+        server.setVpcid(req.getVpcId());
+        server.setNics(List.of(new PostPaidServerNic()
+                .withSubnetId(req.getSubnetId())));
+        server.setFlavorRef(req.getFlavorId());
+        server.setImageRef(req.getImageId());
+        server.setRootVolume(new PostPaidServerRootVolume()
+                .withSize(req.getDataVolumeSize())
+                .withVolumetype(req.getRootVolumeType()));
+        body.setServer(server);
+        createServersRequest.setBody(body);
+        if (req.getEipBandWithInMb() != 0) {
+            server.setPublicip(new PostPaidServerPublicip()
+                    .withDeleteOnTermination(true)
+                    .withEip(new PostPaidServerEip()
+                            .withIptype(req.getEipType())
+                            .withBandwidth(new PostPaidServerEipBandwidth()
+                                    .withChargemode("traffic")
+                                    .withSharetype(PostPaidServerEipBandwidth.SharetypeEnum.PER)
+                                    .withSize(req.getEipBandWithInMb()))));
+        }
+        return ecsClient.createPostPaidServersAsync(createServersRequest);
+    }
+
+    public CompletableFuture<Void> deleteServer(String name) {
         CompletableFuture<Void> future = new CompletableFuture<>();
-        // stop servers. this function only send stop command, do not sync server status
-        stopServers(serverIds).whenComplete((stopServersResponse, e) -> {
-            if (e != null) {
-                throw new RuntimeException(e);
-            }
-            // server status need time to sync
-            // changeos
-            List<CompletableFuture<ChangeServerOsWithoutCloudInitResponse>> futures = new ArrayList<>();
-            for (String serverId : serverIds) {
-                ChangeServerOsWithoutCloudInitRequest changeServerOsRequest =
-                        new ChangeServerOsWithoutCloudInitRequest();
-                changeServerOsRequest.setServerId(serverId);
-                ChangeServerOsWithoutCloudInitRequestBody body = new ChangeServerOsWithoutCloudInitRequestBody();
-                ChangeServerOsWithoutCloudInitOption option = new ChangeServerOsWithoutCloudInitOption();
-                option.setImageid(imageId);
-                body.setOsChange(option);
-                changeServerOsRequest.setBody(body);
-                futures.add(ecsClient.changeServerOsWithoutCloudInitAsync(changeServerOsRequest)
-                        .exceptionally(t -> {
-                            log.info("it seems that ecs server status not sync yet, let's try again after {}s",
-                                    RETRY_PAUSE_TIME / 1000);
-                            if (t instanceof CompletionException completionException
-                                    && completionException.getMessage().contains("Ecs.0100")) {
-                                CommonUtil.sleep(TimeUnit.MILLISECONDS, RETRY_PAUSE_TIME);
-                                futures.add(
-                                        ecsClient.changeServerOsWithoutCloudInitAsync(changeServerOsRequest));
-                            }
-                            return null;
-                        }));
-            }
-            for (CompletableFuture<ChangeServerOsWithoutCloudInitResponse> responseFuture : futures) {
-                try {
-                    responseFuture.get();
-                } catch (InterruptedException | ExecutionException exception) {
-                    throw new RuntimeException(exception);
-                }
-            }
-            future.complete(null);
-        });
+        ecsClient.listServersDetailsAsync(new ListServersDetailsRequest().withName(name))
+                .whenComplete((resp, ex) -> {
+                    if (ex != null) {
+                        throw new RuntimeException(ex);
+                    }
+                    List<ServerId> serverIdList = resp.getServers().stream().map(
+                            serverDetail -> new ServerId().withId(serverDetail.getId())).toList();
+                    DeleteServersRequest deleteServersRequest = new DeleteServersRequest()
+                            .withBody(new DeleteServersRequestBody().withServers(serverIdList));
+                    try {
+                        ecsClient.deleteServersAsync(deleteServersRequest).get();
+                    } catch (Throwable e) {
+                        future.completeExceptionally(e);
+                        throw new RuntimeException(e);
+                    }
+                });
         return future;
     }
 }
